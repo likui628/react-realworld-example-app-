44d38f2ad8af41f543db8b9da7f5dd34f20de76c
likui628 <likui.it@gmail.com>
2023-02-19 21:41:14 +0800

    chore: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home/Home";
-import { Login } from "./pages/Login/Login"
-import { Register } from "./pages/Register/Register"
-import { Settings } from "./pages/Settings/Settings"
-import { EditArticle } from "./pages/EditArticle/EditArticle"
-import { Article } from "./pages/Article/Article"
-import { Profile } from "./pages/Profile/Profile"
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { Settings } from './pages/Settings';
+import { EditArticle } from './pages/EditArticle';
+import { Article } from './pages/Article';
+import { Profile } from './pages/Profile';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/settings" element={<Settings />}></Route>
-        <Route path="/editor" element={<EditArticle />}>   </Route>
-        <Route path="/editor/:slug" element={<EditArticle />}></Route>
-        <Route path="/profile/@:username" element={<Profile />}> </Route>
-        <Route path="/profile/@:username/favorites" element={<Profile />}></Route>
-        <Route path="/article/:slug" element={<Article />}></Route>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/editor" element={<EditArticle />} />
+        <Route path="/editor/:slug" element={<EditArticle />} />
+        <Route path="/profile/@:username" element={<Profile />} />
+        <Route path="/profile/@:username/favorites" element={<Profile />} />
+        <Route path="/article/:slug" element={<Article />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </HashRouter>
   );
--- src/config.ts
+++
@@ -1,3 +0,0 @@
-export const CONFIG = {
-  API_HOST: 'https://api.realworld.io/api/',
-}
