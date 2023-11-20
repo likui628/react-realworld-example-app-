637a1425c30c3a67051c9c44175c3b0316fa55e4
likui628 <90845831+likui628@users.noreply.github.com>
2023-11-20 15:02:21 +0800

    feat: Header navigation bar displays different content based on user login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,53 @@
 import { Link } from 'react-router-dom';
+import { useAppSelector } from '../state/storeHooks';
 export function Header() {
+  const user = useAppSelector(state => state.auth.user);
   return (
     <nav className="navbar navbar-light">
       <div className="container">
-        <Link to={''} className="navbar-brand">
+        <Link to={'/'} className="navbar-brand">
           conduit
         </Link>
         <ul className="nav navbar-nav pull-xs-right">
           <li className="nav-item">
-            <Link to={''} className="nav-link">
+            <Link to={'/'} className="nav-link">
               Home
             </Link>
           </li>
-          <li className="nav-item">
-            <Link to={'editor'} className="nav-link">
-              <i className="ion-compose"></i>&nbsp;New Post
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={'settings'} className="nav-link">
-              <i className="ion-gear-a"></i>&nbsp;Settings
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={'/register'} className="nav-link">
-              Sign up
-            </Link>
-          </li>
+          {user ? (
+            <>
+              <li className="nav-item">
+                <Link to={'/editor'} className="nav-link">
+                  <i className="ion-compose"></i>&nbsp;New Post
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={'/settings'} className="nav-link">
+                  <i className="ion-gear-a"></i>&nbsp;Settings
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to={`/profile/@${user?.username}`} className="nav-link">
+                  {user.image && <img src={user.image} className="user-pic" />}
+                  {user.username}
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="nav-item">
+                <Link to={'/login'} className="nav-link">
+                  Sign in
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={'/register'} className="nav-link">
+                  Sign up
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </nav>
