8b60f8ccb4e29d982f45af14c67a2ca66c458ceb
likui628 <90845831+likui628@users.noreply.github.com>
2023-11-21 16:39:34 +0800

    chore: suppress warning

--- src/pages/Article/components/ArticleComments.tsx
+++ src/pages/Article/components/ArticleComments.tsx
@@ -61,43 +61,41 @@ export function ArticleComments({ slug }: ArticleCommentsProps) {
             </form>
 
             {comments.map(comment => (
-              <>
-                <div className="card" key={comment.id}>
-                  <div className="card-block">
-                    <p className="card-text">{comment.body}</p>
-                  </div>
-                  <div className="card-footer">
-                    <Link
-                      to={`/profile/${comment.author.username}`}
-                      className="comment-author"
-                    >
-                      <img
-                        alt=""
-                        src={comment.author.image}
-                        className="comment-author-img"
-                      />
-                    </Link>
-                    &nbsp;
-                    <Link
-                      to={`/profile/${comment.author.username}`}
-                      className="comment-author"
-                    >
-                      {comment.author.username}
-                    </Link>
-                    <span className="date-posted">
-                      {formatTime(comment.updatedAt)}
-                    </span>
-                    {comment.author.username === user.username && (
-                      <span
-                        className="mod-options"
-                        onClick={() => onDeleteComment(comment.id)}
-                      >
-                        <i className="ion-trash-a"></i>
-                      </span>
-                    )}
-                  </div>
+              <div className="card" key={comment.id}>
+                <div className="card-block">
+                  <p className="card-text">{comment.body}</p>
                 </div>
-              </>
+                <div className="card-footer">
+                  <Link
+                    to={`/profile/${comment.author.username}`}
+                    className="comment-author"
+                  >
+                    <img
+                      alt=""
+                      src={comment.author.image}
+                      className="comment-author-img"
+                    />
+                  </Link>
+                  &nbsp;
+                  <Link
+                    to={`/profile/${comment.author.username}`}
+                    className="comment-author"
+                  >
+                    {comment.author.username}
+                  </Link>
+                  <span className="date-posted">
+                    {formatTime(comment.updatedAt)}
+                  </span>
+                  {comment.author.username === user.username && (
+                    <span
+                      className="mod-options"
+                      onClick={() => onDeleteComment(comment.id)}
+                    >
+                      <i className="ion-trash-a"></i>
+                    </span>
+                  )}
+                </div>
+              </div>
             ))}
           </>
         ) : (
